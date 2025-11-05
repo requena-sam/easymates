c02ed214561755d77e5ab6f0d206a4fffacad675
requena-sam <145543184+requena-sam@users.noreply.github.com>
2025-11-05 15:57:58 +0100

    Install Pest

--- tests/Feature/Auth/PasswordConfirmationTest.php
+++ tests/Feature/Auth/PasswordConfirmationTest.php
@@ -1,21 +1,13 @@
 <?php
 
-namespace Tests\Feature\Auth;
-
 use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
 
-class PasswordConfirmationTest extends TestCase
-{
-    use RefreshDatabase;
+uses(\Illuminate\Foundation\Testing\RefreshDatabase::class);
 
-    public function test_confirm_password_screen_can_be_rendered(): void
-    {
-        $user = User::factory()->create();
+test('confirm password screen can be rendered', function () {
+    $user = User::factory()->create();
 
-        $response = $this->actingAs($user)->get(route('password.confirm'));
+    $response = $this->actingAs($user)->get(route('password.confirm'));
 
-        $response->assertStatus(200);
-    }
-}
+    $response->assertStatus(200);
+});
