12e19a8c8e7af62603b3d3ceb0e6b947302f66b5
requena-sam <145543184+requena-sam@users.noreply.github.com>
2026-01-18 22:54:14 +0100

    Open Graph

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,5 +1,19 @@
-<meta charset="utf-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta charset="utf-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<!-- Open Graph -->
+<meta property="og:title" content="Easy Mates">
+<meta property="og:description"
+      content="L’application qui facilite la vie des fans de Gentle Mates. Partage tes créations, prévois tes événemments et restent connecté avec tous les joueurs de la structure !.">
+<meta property="og:image" content="{{ asset('storage/images/logo_gentlemates.svg') }}">
+<meta property="og:url" content="https://easymates.laravel.cloud">
+<meta property="og:type" content="website">
+
+<!-- Twitter -->
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:title" content="Easy Mates">
+<meta name="twitter:description" content="Facilite ta vie en tant que fan de Gentle Mates.">
+<meta name="twitter:image" content="{{ asset('storage/images/logo_gentlemates.svg') }}">
+
 
 <title>{{ config('app.name') }}</title>
 
@@ -8,6 +22,6 @@
 <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
 <link rel="preconnect" href="https://fonts.bunny.net">
-<link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+<link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet"/>
 
 @vite(['resources/css/app.css', 'resources/js/app.js'])
