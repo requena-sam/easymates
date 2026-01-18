a140954531dffadafc2ceb5766fdda4d13c57ed5
requena-sam <145543184+requena-sam@users.noreply.github.com>
2026-01-18 16:38:56 +0100

    Updating seeding class

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,8 +23,6 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= 'password',
             'remember_token' => Str::random(10),
@@ -36,7 +34,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
