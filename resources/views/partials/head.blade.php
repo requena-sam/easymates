337176b10e1607d44af79e29293022ccbda192c4
requena-sam <145543184+requena-sam@users.noreply.github.com>
2026-01-18 23:30:22 +0100

    Open Graph

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -4,7 +4,7 @@
 <meta property="og:title" content="Easy Mates">
 <meta property="og:description"
       content="L’application qui facilite la vie des fans de Gentle Mates. Partage tes créations, prévois tes événemments et restent connecté avec tous les joueurs de la structure !.">
-<meta property="og:image" content="{{ asset('storage/images/logo_gentlemates.png') }}">
+<meta property="og:image" content="{{ asset('storage/images/og_image.png') }}">
 <meta property="og:url" content="https://easymates.laravel.cloud">
 <meta property="og:type" content="website">
 
